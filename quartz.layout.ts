8dd3b94c511e194f1fa7b232c35f37e967df100d
9-op-9 <mail@oliverprechtl.de>
2026-05-27 15:15:30 +0200

    Quartz sync: May 27, 2026, 3:15 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,10 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   afterBody: [],
   footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
+    links: {},
   }),
 }
 
@@ -38,7 +35,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({ title: "Menü" }),
   ],
   right: [
     Component.Graph(),
